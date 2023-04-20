2f1e00798fcac06047f9c85cfb7aa83e0f017b9d
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-04-20 15:43:45 +0300

    create programm for Task8

---
+++ task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int number = 0;
+
+while(number <= a) {
+    if((number % 2) == 0){
+        Console.Write($"{number} ");
+    }
+    number++;
+}
+
+Console.Write("\n");
