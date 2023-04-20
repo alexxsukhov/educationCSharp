29aba223f5411028aa2ef3379a956f8988577e9e
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-04-20 16:17:14 +0300

    change output string

--- task8/Program.cs
+++ task8/Program.cs
@@ -10,7 +10,7 @@ int number = 1;
 
 while(number <= a) {
     if((number % 2) == 0){
-        if(number == a) Console.Write($"{number}");
+        if(number == a || number == a - 1) Console.Write($"{number}");
         else Console.Write($"{number}, ");
     }
     number++;
