37aea86e43753a5b8cc764d64f63e62a82077fb3
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-05-01 19:01:30 +0300

    create task 19 for seminar 3

---
+++ seminar_3/task_19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int reverse_number = 0;
+int copy_number = number;
+
+while (copy_number > 0)
+{    
+    int end_numeric = copy_number % 10;
+    copy_number = copy_number / 10;
+    reverse_number = reverse_number * 10 + end_numeric;
+}
+
+if(number == reverse_number) {
+    Console.WriteLine("Число является палиндромом");
+}else{
+    Console.WriteLine("Число не является палиндромом");
+}
