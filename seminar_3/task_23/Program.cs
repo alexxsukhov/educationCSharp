dc0e84a2d68f957fa57374857798e6b437d23359
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-05-02 17:31:07 +0300

    update task 23

--- seminar_3/task_23/Program.cs
+++ seminar_3/task_23/Program.cs
@@ -9,5 +9,5 @@ Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 for(int i = 1; i <= number; i++){
-    Console.WriteLine(Math.Pow(i, 4));
+    Console.WriteLine(Math.Pow(i, 3));
 }
