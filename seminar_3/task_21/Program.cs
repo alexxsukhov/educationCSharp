49362fc9cc2504889db459a635a877a3d8215c51
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-05-02 12:17:33 +0300

    done task 21 for seminar 3

---
+++ seminar_3/task_21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите координату x для первой точки: ");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y для первой точки: ");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z для первой точки: ");
+double az = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату x для второй точки: ");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y для второй точки: ");
+double by = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z для второй точки: ");
+double bz = Convert.ToDouble(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2) + Math.Pow((bz - az), 2));
+
+Console.WriteLine($"Расстояние между точками в трехмерном пространстве: {distance}");
