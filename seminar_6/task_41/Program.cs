7e2e4034f4a2cbb26bc15bf5ed5de920044953f3
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-05-29 12:46:11 +0300

    add task 41

---
+++ seminar_6/task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int count = 0;
+
+while (true)
+{
+    Console.WriteLine("Введите число: ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber > 0)
+    {
+        count = count + 1;
+    }
+
+    if (userNumber == 0)
+    {
+        break;
+    }
+}
+Console.WriteLine($"Количество положительных чисел: {count}");
+
+
+
