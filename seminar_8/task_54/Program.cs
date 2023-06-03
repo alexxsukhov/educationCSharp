0819d2bd5e20c54ce64ca2e9e71d5fb942b5a335
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-06-03 14:57:23 +0300

    add tasks 54 and 56 for seminar 8

---
+++ seminar_8/task_54/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+ */
+ 
+//Генерация двумерного массива
+int[,] GenerateMatrix(int rows = 4, int cols = 4, int start = 1, int end = 10)
+{
+    int[,] array = new int[rows, cols];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(start, end);
+        }
+    }
+
+    return array;
+}
+
+
+//Вывод двумерного массива на печать
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+void SortMatrixRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int x = 0; x < array.GetLength(1) - 1; x++)
+            {
+                if (array[i, x] < array[i, x + 1])
+                {
+                    (array[i, x], array[i, x + 1]) = (array[i, x + 1], array[i, x]);
+                }
+            }
+        }
+    }
+}
+
+int[,] userMatrix = GenerateMatrix();
+
+PrintMatrix(userMatrix);
+SortMatrixRows(userMatrix);
+Console.WriteLine();
+PrintMatrix(userMatrix);
---
+++ seminar_8/task_56/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+ */
+
+
+//Генерация двумерного массива
+int[,] GenerateMatrix(int rows = 4, int cols = 4, int start = 0, int end = 10)
+{
+    int[,] array = new int[rows, cols];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(start, end);
+        }
+    }
+
+    return array;
+}
+ 
+//Вывод двумерного массива на печать
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+void FindMinimalSummaryRow(int[,] array)
+{
+    int rowSummary = 0;
+    int numberLine = 0;
+    int minimalSummary = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            rowSummary += array[i, j];
+            if (i == 0)
+            {
+                minimalSummary = rowSummary;
+            }
+        }
+
+        if (minimalSummary > rowSummary)
+        {
+            minimalSummary = rowSummary;
+            numberLine = i;
+        }
+        rowSummary = 0;
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов {numberLine} - сумма элементов {minimalSummary}");
+}
+
+int[,] userMatrix = GenerateMatrix();
+
+PrintMatrix(userMatrix);
+Console.WriteLine();
+FindMinimalSummaryRow(userMatrix);
