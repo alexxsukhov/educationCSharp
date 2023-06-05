9ddf72378ceaa6bd308703a74d0c82f008b6be83
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-06-05 13:31:20 +0300

    add tasks for seminar 9

---
+++ seminar_9/task_66/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+int SummaryNumberInRange(int start = 1, int end = 5)
+{
+    if (start == end)
+    {
+        return end;
+    }
+
+    return start + SummaryNumberInRange(start + 1, end);
+}
+
+Console.WriteLine(SummaryNumberInRange());
